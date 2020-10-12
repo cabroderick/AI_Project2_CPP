fb20b0f8e29e6635fff790a6f5f16f8c8247e686
cbroderick-PC\cbroderick <sedrahedron@gmail.com>
2020-10-12 16:14:13 -0400

    Finished up BoardConfiguration

--- AI_Project2_CPP/BoardConfiguration.cpp
+++ AI_Project2_CPP/BoardConfiguration.cpp
@@ -1,12 +1,26 @@
 #include <vector>
+#include <BoardConfiguration.h>
 
 using namespace std;
 
 class BoardConfiguration {
 public:
   BoardConfiguration (int x, int y) {
+    boardSize = 15;
     //do stuff here
   }
-private:
+  BoardConfiguration placeStone(int col, int inputRow, int player) {
+    boardList[col][row] = player;
+      occupiedSpaces.push_back(make_pair(col,row));
 
+
+private:
+  //initializes the board
+  void initBoardRange() {
+    for (size_t col = 0; cow < boardSize; col++) {
+        for (size_t row = 0; row < boardSize; row++) {
+          boardList[col][row] = 0;
+        }
+    }
+  }
 }
--- AI_Project2_CPP/BoardConfiguration.h
+++ AI_Project2_CPP/BoardConfiguration.h
@@ -4,22 +4,11 @@ using namespace std;
 
 class BoardConfiguration {
 private:
-  int boardSize = 15;
+  int boardSize;
   vector<vector<int>> boardList;
   vector<pair<int,int>> occupiedSpaces;
+  void initBoardRange();
 
-
+public:
+  BoardConfiguration();
 }
-
-class BoardConfiguration{
-
-
-
-    occupiedSpaces = [] //list of spaces occupied on the board
-    def __init__(self):
-        self.boardList = [[0 for x in range(self.boardSize)] for y in range(self.boardSize)]
-    def placeStone(self, col: int, row: int, player: int):
-        self.boardList[col][row] = player
-        self.occupiedSpaces.append(Vector(col, row))
-        return self
-};
